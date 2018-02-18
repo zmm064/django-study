65c2038d875e2e22f1edf076a3daa72b5c50c2aa
xuhui <zmm064@foxmail.com>
2018-02-18 19:56:16 +0800

    课时25：实现内置的退出方法

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     # url(r'^login/$', views.user_login, name="user_login"),
     url(r'^login/$', auth_views.login, {"template_name": "account/login.html"}, name="user_login"),
+    url(r'^logout/$', auth_views.logout, {"template_name": "account/logout.html"}, name="user_logout"),
 ]
