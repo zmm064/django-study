26c1468e6f56c826d8068f3070340b52c4689132
xuhui <zmm064@foxmail.com>
2018-02-20 09:46:34 +0800

    课时31：用django 自带方法实现修改密码

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, {"template_name": "account/login.html"}, name="user_login"),
     url(r'^logout/$', auth_views.logout, {"template_name": "account/logout.html"}, name="user_logout"),
     url(r'^register/$', views.register, name="user_register"),
+    url(r'^password-change/$', auth_views.password_change, {"post_change_redirect":"/account/password-change-done"}, name="password_change"),
+    url(r'^password-change-done/$', auth_views.password_change_done, name="password_change_done"),
 ]
