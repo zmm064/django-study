d78af0367b33323c539fb55a46029ffb0dab6f5a
xuhui <zmm064@foxmail.com>
2018-02-22 09:28:50 +0800

    课时44：删除栏目功能实现

--- mysite/article/views.py
+++ mysite/article/views.py
@@ -12,32 +12,44 @@ from .forms import ArticleColumnForm
 @login_required(login_url='/account/login/')
 @csrf_exempt
 def article_column(request):
-	if request.method == "GET":
-		columns = ArticleColumn.objects.filter(user=request.user)
-		column_form = ArticleColumnForm()
-		return render(request, 
-					  "article/column/article_column.html", 
-					  {"columns": columns, 'column_form':column_form})
-	if request.method == "POST":
-		column_name = request.POST['column']
-		columns = ArticleColumn.objects.filter(user_id=request.user.id, column=column_name)
-		if columns:
-			return HttpResponse('2')
-		else:
-			# 新增栏目并返回创建成功的响应
-			ArticleColumn.objects.create(user=request.user, column=column_name)
-			return HttpResponse("1")
-	
+    if request.method == "GET":
+        columns = ArticleColumn.objects.filter(user=request.user)
+        column_form = ArticleColumnForm()
+        return render(request, 
+                      "article/column/article_column.html", 
+                      {"columns": columns, 'column_form':column_form})
+    if request.method == "POST":
+        column_name = request.POST['column']
+        columns = ArticleColumn.objects.filter(user_id=request.user.id, column=column_name)
+        if columns:
+            return HttpResponse('2')
+        else:
+            # 新增栏目并返回创建成功的响应
+            ArticleColumn.objects.create(user=request.user, column=column_name)
+            return HttpResponse("1")
+    
 @login_required(login_url='/account/login')
 @require_POST
 @csrf_exempt
 def rename_article_column(request):
-	column_name = request.POST["column_name"]
-	column_id = request.POST["column_id"]
-	try:
-		line = ArticleColumn.objects.get(id=column_id)
-		line.column = column_name
-		line.save()
-		return HttpResponse("1")
-	except:
-		return HttpResponse("0")
+    column_name = request.POST["column_name"]
+    column_id = request.POST["column_id"]
+    try:
+        line = ArticleColumn.objects.get(id=column_id)
+        line.column = column_name
+        line.save()
+        return HttpResponse("1")
+    except:
+        return HttpResponse("0")
+
+@login_required(login_url='/account/login')
+@require_POST
+@csrf_exempt
+def del_article_column(request):
+    column_id = request.POST["column_id"]
+    try:
+        line = ArticleColumn.objects.get(id=column_id)
+        line.delete()
+        return HttpResponse("1")
+    except:
+        return HttpResponse("2")
