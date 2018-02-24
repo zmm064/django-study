5f6c5bca4bd2098cb31cb41efe081443c1ee2595
xuhui <zmm064@foxmail.com>
2018-02-24 09:42:58 +0800

    课时60：实现显示特定作者的文章列表页

--- mysite/article/list_views.py
+++ mysite/article/list_views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import ArticleColumn, ArticlePost
+from django.contrib.auth.models import User
 
 
-def article_titles(request):
-    article_title = ArticlePost.objects.all()
+def article_titles(request, username=None):
+    if username:
+        user = User.objects.get(username=username)
+        article_title = ArticlePost.objects.filter(author=user)
+        try:
+            userinfo = user.userinfo
+        except:
+            userinfo = None
+    else:
+        article_title = ArticlePost.objects.all()
+
     paginator = Paginator(article_title, 2)
     page = request.GET.get('page')
     try:
@@ -16,10 +26,14 @@ def article_titles(request):
     except EmptyPage:
         current_page = paginator.page(paginator.num_pages)
         articles = current_page.object_list
-    return render(request, 
-                  "article/list/article_titles.html",
-                  {"articles":articles, "page":current_page})
 
+    if username:
+        return render(request, 
+                      "article/list/author_articles.html",
+                      {"articles":articles, "page":current_page, "userinfo":userinfo, "user":user})
+    return render(request, 
+                  "article/list/article_titles.html", 
+                  {"articles":articles, "page": current_page})
 
 def article_detail(request, id, slug):
     article = get_object_or_404(ArticlePost, id=id, slug=slug)
--- mysite/article/views.py
+++ mysite/article/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.models import User
 
 from .models import ArticleColumn, ArticlePost
 from .forms import ArticleColumnForm, ArticlePostForm
