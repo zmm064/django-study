ed8a13a6f03ab4b513285c544a9c6fe8b634dc57
xuhui <zmm064@foxmail.com>
2018-02-26 14:30:25 +0800

    课时75：使用inclusion_tag实现展示最新文章

--- mysite/article/templatetags/article_tags.py
+++ mysite/article/templatetags/article_tags.py
@@ -1,6 +1,6 @@
 from django import template 
 from article.models import ArticlePost
-
+#创建了一个实例对象register，这个对象包含了simple_tag方法（以及另外两种）
 register = template.Library()
 
 
@@ -11,3 +11,11 @@ def total_articles():
 @register.simple_tag
 def author_total_articles(user):
     return user.article.count()
+
+
+@register.inclusion_tag('article/list/latest_articles.html')  # 确定所渲染的模板文件
+def latest_articles(n=5):
+    latest_articles = ArticlePost.objects.order_by("-created")[:n]
+    # 返回字典类型的数据，此数据被应用到上面所指定的模板文件中
+    return {"latest_articles": latest_articles}
+
