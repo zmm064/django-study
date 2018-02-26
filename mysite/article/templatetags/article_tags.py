ff4699592cec05de6674d6e099d178cc78a84c88
xuhui <zmm064@foxmail.com>
2018-02-26 14:48:52 +0800

    课时76：评论最多的文章

--- mysite/article/templatetags/article_tags.py
+++ mysite/article/templatetags/article_tags.py
@@ -1,5 +1,6 @@
 from django import template 
 from article.models import ArticlePost
+from django.db.models import Count
 #创建了一个实例对象register，这个对象包含了simple_tag方法（以及另外两种）
 register = template.Library()
 
@@ -19,3 +20,8 @@ def latest_articles(n=5):
     # 返回字典类型的数据，此数据被应用到上面所指定的模板文件中
     return {"latest_articles": latest_articles}
 
+
+@register.assignment_tag
+def most_commented_articles(n=3):
+    return ArticlePost.objects.annotate(
+        total_comments=Count('comments')).order_by("-total_comments")[:n]
