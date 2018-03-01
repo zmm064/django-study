7838dd2eeb329cbe178af2f69056c5705b710662
xuhui <zmm064@foxmail.com>
2018-03-01 15:19:43 +0800

    课时93：使用ListView读取并展示数据

--- mysite/course/views.py
+++ mysite/course/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, ListView    
 
-# Create your views here.
+from .models import Course    
+
+class CourseListView(ListView):    
+    model = Course    
+    # 声明传入模板的变量名称。如果不写，模板默认变量名称是object
+    # 变量指代相应数据表中的所有记录
+    context_object_name = "courses"    
+    template_name = 'course/course_list.html'
