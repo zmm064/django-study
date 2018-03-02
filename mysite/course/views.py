77c1c5c0552808208a944cfceb14f125ab214c21
xuhui <zmm064@foxmail.com>
2018-03-02 08:49:31 +0800

    课时94：初步了解Mixin

--- mysite/course/views.py
+++ mysite/course/views.py
@@ -1,9 +1,16 @@
 from django.views.generic import TemplateView, ListView    
 
-from .models import Course    
+from .models import Course
 
-class CourseListView(ListView):    
-    model = Course    
+class UserMixin:
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(user=self.request.user)
+
+class UserCourseMixin(UserMixin):
+    model = Course
+
+class CourseListView(UserCourseMixin, ListView):    # 一般将Mixin类放在左边，其它类放在右边
     # 声明传入模板的变量名称。如果不写，模板默认变量名称是object
     # 变量指代相应数据表中的所有记录
     context_object_name = "courses"    
