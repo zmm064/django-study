4bb9edc51020100131080500252a24c3683c2781
xuhui <zmm064@foxmail.com>
2018-03-02 09:55:14 +0800

    课时96：使用第三方MixIn判断用户是否登录

--- mysite/course/urls.py
+++ mysite/course/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from .views import CourseListView
+from .views import CourseListView, ManageCourseListView
 
 urlpatterns = [
     url(r'about/$', TemplateView.as_view(template_name="course/about.html"), name='about'),
     url(r'course-list/$', CourseListView.as_view(), name="course_list"),
+    url(r'manage-course/$', ManageCourseListView.as_view(), name="manage_course"),
 ]
--- mysite/course/views.py
+++ mysite/course/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView, ListView    
+from django.views.generic import TemplateView, ListView
+from braces.views import LoginRequiredMixin
 
 from .models import Course
 
@@ -7,11 +8,18 @@ class UserMixin:
         qs = super().get_queryset()
         return qs.filter(user=self.request.user)
 
-class UserCourseMixin(UserMixin):
+class UserCourseMixin(UserMixin, LoginRequiredMixin):
     model = Course
+    login_url = "/account/login/"
 
-class CourseListView(UserCourseMixin, ListView):    # 一般将Mixin类放在左边，其它类放在右边
+class CourseListView(ListView):
     # 声明传入模板的变量名称。如果不写，模板默认变量名称是object
     # 变量指代相应数据表中的所有记录
+    model = Course
     context_object_name = "courses"    
     template_name = 'course/course_list.html'
+
+
+class ManageCourseListView(UserCourseMixin, ListView):  # 一般将Mixin类放在左边，其它类放在右边
+    context_object_name = "courses"    
+    template_name = 'course/manage/manage_course_list.html'
